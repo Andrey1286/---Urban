314b23f7ffa7691881679d9197b35adad844f28c
Andrey1286 <Andrej000022@yandex.ru>
2024-08-24 21:05:06 +0300

    Add files via upload

--- module_2_hard.py
+++ module_2_hard.py
@@ -1,10 +1,14 @@
-def generate_password(n):
-    soda1 = []
-    for i in range(1, n):
-        for j in range(1, n):
-            if n % (i + j) == 0:
-                soda1.append(str(i) + str(j))
-    result = ''.join(soda1)
-    return result
-n = int(input("Введите число от 3 до 20: "))
-print("Пароль:", generate_password(n))
+cod = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+spisok1 = []
+
+n = int(input('Введите число от 3 до 20: '))
+
+for i in range(len(cod)):
+     for j in range(i + 1, len(cod)):
+         sum1 = cod[i] + cod[j]
+         if n % sum1 == 0:
+             spisok1.append(str(cod[i]) + str(cod[j]))
+
+
+spisok2 = int(''.join(spisok1))
+print('Сгенерированный пароль:', spisok2)
