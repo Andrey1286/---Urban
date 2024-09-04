97c1960529266fc0c43e6ac45dc60ce6e011c795
Andrey1286 <Andrej000022@yandex.ru>
2024-09-04 22:39:10 +0300

    Add files via upload

--- module_3_hard.py
+++ module_3_hard.py
@@ -1,15 +1,8 @@
-data_structure = [
-        [1, 2, 3],
-        {'a': 4, 'b': 5},
-        (6, {'cube': 7, 'drum': 8}),
-        "Hello",
-        ((), [{(2, 'Urban', ('Urban2', 35))}])]
-
-stroka = 1
 
 def calculate_structure_sum(*arg):
     list_of_numbers = []
-    for i in data_structure:
+    x = 0
+    for i in arg:
         if isinstance(i, str):
             list_of_numbers.append(stroka)
         elif isinstance(i, list):
@@ -32,9 +25,21 @@ def calculate_structure_sum(*arg):
                     list_of_numbers.append(stroka)
                 if isinstance(f, int):
                     list_of_numbers.append(f)
+        if x == 0:
+            print(list_of_numbers)
+            exit()
+        x += 1
 
     return sum(list_of_numbers)
 
+data_structure = [
+        [1, 2, 3],
+        {'a': 4, 'b': 5},
+        (6, {'cube': 7, 'drum': 8}),
+        "Hello",
+        ((), [{(2, 'Urban', ('Urban2', 35))}])]
+
+stroka = 1
 
 result = calculate_structure_sum(data_structure)
 print(result)
