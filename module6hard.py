664e23014196993cc6121528228650a6dc999ba0
Andrey1286 <Andrej000022@yandex.ru>
2024-10-14 16:20:12 +0300

    Add files via upload

--- module6hard.py
+++ module6hard.py
@@ -2,52 +2,57 @@ from math import *
 
 class Figure:
     sides_count = 0
-    def __init__(self, color,  *sides):
-        self.__sides = []
-        self.__color = color           # список цветов
-        self.filled = True  # bool - тип данных, который может принимать True или False
+    def __init__(self, color,  *sides: int):
+        self.__sides = list(sides)           # список сторон
+        self.__color = list(color)           # список цветов
+        self.filled = True                   # bool - тип данных, который может принимать True или False
 
     def get_color(self):
-        return self.__color
+        return self.__color                  # возвращает список цветов
 
-    def __is_valid_color(self, r, g, b):
-        return all(isinstance(value, int) and 0 <= value <= 255 for value in [r, g, b])
+    def __is_valid_color(self, *rgb):
+        if len(rgb) == 3:
+            for i in (rgb):
+                if 0 <= i <= 255:
+                    return True
 
     def set_color(self, r, g, b):
         if self.__is_valid_color(r, g, b):
             self.__color = [r, g, b]
         else:
-            self.__color = self.__color
             print('Цвет остаётся прежним')
 
     def __is_valid_sides(self, *sides):
-        if all(isinstance(side, int) and side > 0 for side in sides):
-            return True
-        else:
-            False
+        for i in sides:
+            if not isinstance(i, int) or i <= 0:
+                return False
+        return True
 
     def get_sides(self):
         return self.__sides
 
-    def __len__(self):
-        return sum(self.__sides)
+    def __len__(self):  # возвращает периметр фигуры
+        if len(self.__sides) == 1:
+            x = self.__sides[0]
+        else:
+            x = sum(self.__sides)
+        return x  # периметр фигуры
 
     def set_sides(self, *new_sides):
         if len(new_sides) == self.sides_count and self.__is_valid_sides(*new_sides):
             self.__sides = list(new_sides)
-        else:
-            print('Неверное количество или тип сторон')
 
 class Circle(Figure):
     sides_count = 1
     def __init__(self, color,  *sides):
-        pi = 3.14159
         super().__init__(color,  *sides)
-        self.__radius = sides / (2 * pi)
+        if len(sides) == 1:
+            self.__radius = sides[0] / (2 * pi)
+        else:
+            self.__radius = 1 / (2 * pi)
 
     def get_square(self):
-        pi = 3.14159
-        circle_area = pi * self.__radius ** 2
+        circle_area = pi * (self.__radius ** 2)
         return circle_area
 
 class Triangle(Figure):
@@ -56,25 +61,27 @@ class Triangle(Figure):
         super().__init__(color,  *sides)
 
     def get_square(self):
-        p = Figure.__len__() / 2  # полупериметр
-        a = self.get_sides([0])
-        b = self.get_sides([1])
-        c = self.get_sides([2])
-        circle_area = math.sqrt(p * (p - a) * (p - b) * (p-c))
+        p = self.__len__() / 2  # полупериметр
+        a = self.get_sides()[0]  # длина первой стороны
+        b = self.get_sides()[1]  # длина второй стороны
+        c = self.get_sides()[2]  # длина третьей стороны
+        circle_area = sqrt(p * (p - a) * (p - b) * (p-c))  # считаем площадь треугольнака по формуле Герона
         return circle_area
 
 class Cube(Figure):
     sides_count = 12
     def __init__(self, color,  *sides):
+        if len(sides) == 1:
+            sides = [sides[0]] * self.sides_count
+        else:
+            sides = [1] * self.sides_count
         super().__init__(color,  *sides)
-        self.side_length = self.get_sides([0])
-        x = self.get_sides()
-        for i in range(12):
-            x.append(i)
+
 
     def get_volume(self):
-        cube_volume = self.side_length ** 3
+        cube_volume = self.get_sides()[0] ** 3
         return cube_volume
+
 
 circle1 = Circle((200, 200, 100), 10) # (Цвет, стороны)
 cube1 = Cube((222, 35, 130), 6)
