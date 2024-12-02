1e9a8f5597ae348b1646e59689260c9b9e7bd1ac
Andrey1286 <Andrej000022@yandex.ru>
2024-12-02 12:38:48 +0300

    Add files via upload

--- module_5_3.py
+++ module_5_3.py
@@ -1,13 +1,13 @@
 class House:
-    def __init__(self, name, number_of_floors):
+    def __init__(self, name, number_of_floors: int):
         self.name = name
         self.number_of_floors = number_of_floors
 
-    def go_to(self, new_floor):
+    def go_to(self, new_floor: int):
         if new_floor > self.number_of_floors or new_floor < 1:
-            print('Такого этажа не существует')
-            return
-        for i in range(1, new_floor + 1):
+            print(f'Такого этажа не существует')
+        else:
+            for i in range(1, new_floor + 1):
                 print(i)
 
     def __len__(self):
@@ -17,47 +17,52 @@ class House:
         return f'Название: {self.name}, кол-во этажей: {self.number_of_floors}'
 
     def __eq__(self, other):
-        if isinstance(other, House):
-            return self.number_of_floors == other.number_of_floors
-        if isinstance(other, int):
-            return self.number_of_floors == other
-        return False
-
+        if isinstance(other, House) and self.number_of_floors == other.number_of_floors:
+            return True
+        elif isinstance(other, int) and self.number_of_floors == other:
+            return True
+        else:
+            return False
 
     def __lt__(self, other):
-        if isinstance(other, House):
-            return self.number_of_floors < other.number_of_floors
-        if isinstance(other, int):
-            return self.number_of_floors < other
-        return False
-
-    def __le__(self, other):
-        if isinstance(other, House):
-            return self.number_of_floors <= other.number_of_floors
-        if isinstance(other, int):
-            return self.number_of_floors <= other
-        return False
+        if isinstance(other, House) and self.number_of_floors < other.number_of_floors:
+            return True
+        elif isinstance(other, int) and self.number_of_floors < other:
+            return True
+        else:
+            return False
 
     def __gt__(self, other):
-        if isinstance(other, House):
-            return self.number_of_floors > other.number_of_floors
-        if isinstance(other, int):
-            return self.number_of_floors > other
-        return False
+        if isinstance(other, House) and self.number_of_floors > other.number_of_floors:
+            return True
+        elif isinstance(other, int) and self.number_of_floors > other:
+            return True
+        else:
+            return False
+
+    def __le__(self, other):
+        if isinstance(other, House) and self.number_of_floors <= other.number_of_floors:
+            return True
+        elif isinstance(other, int) and self.number_of_floors <= other:
+            return True
+        else:
+            return False
 
     def __ge__(self, other):
-        if isinstance(other, House):
-            return self.number_of_floors >= other.number_of_floors
-        if isinstance(other, int):
-            return self.number_of_floors >= other
+        if isinstance(other, House) and self.number_of_floors >= other.number_of_floors:
+            return True
+        elif isinstance(other, int) and self.number_of_floors >= other:
+            return True
+        else:
             return False
 
     def __ne__(self, other):
-        if isinstance(other, House):
-            return self.number_of_floors != other.number_of_floors
-        if isinstance(other, int):
-            return self.number_of_floors != other
-        return False
+        if isinstance(other, House) and self.number_of_floors != other.number_of_floors:
+            return True
+        elif isinstance(other, int) and self.number_of_floors != other:
+            return True
+        else:
+            return False
 
     def __add__(self, value):
         if isinstance(value, int):
@@ -72,21 +77,33 @@ class House:
 
 
 h1 = House('ЖК Эльбрус', 10)
+
 h2 = House('ЖК Акация', 20)
 
+h1 = House('ЖК Эльбрус', 10)
+
+h2 = House('ЖК Акация', 20)
+
+
+
 print(h1)
+
 print(h2)
 
 print(h1 == h2) # __eq__
 
 h1 = h1 + 10 # __add__
+
 print(h1)
+
 print(h1 == h2)
 
 h1 += 10 # __iadd__
+
 print(h1)
 
 h2 = 10 + h2 # __radd__
+
 print(h2)
 
 print(h1 > h2) # __gt__
